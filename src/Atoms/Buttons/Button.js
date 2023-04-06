14160435f4388d470f9355fa3a6d17953833eaaf
shoaib <shoaibmohd916@gmail.com>
2023-04-06 22:58:02 +0530

    Adding module css

---
+++ src/Atoms/Buttons/Button.js
@@ -0,0 +1,15 @@
+import button from './button.module.css'
+
+const Button = () => {
+    return(
+        <div className={button.main__Component}>
+        
+        <button> + </button>
+
+        </div>
+    )
+
+
+}
+
+export default Button
