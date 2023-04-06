b4493f575544964eb7ecbbfdc542f9eb79aa2271
shoaib <shoaibmohd916@gmail.com>
2023-04-06 22:52:14 +0530

    Created inputs and buttons

---
+++ src/Atoms/Input/Input.js
@@ -0,0 +1,12 @@
+import input from './input.module.css'
+
+const Input = () => {
+    return (
+        <div className={input.main__Component}>
+            <input className={input.input_Field} type='text' />
+
+        </div>
+    )
+}
+
+export default Input
